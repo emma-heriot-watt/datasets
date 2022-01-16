556d2e86971b6287b0aaea551acb22fa579dc203
Amit Parekh <7276308+amitkparekh@users.noreply.github.com>
2022-01-16 18:43:51 +0000

    Read CSV files as a python dict (#63)

---
+++ src/io/csv.py
@@ -0,0 +1,17 @@
+import csv
+from pathlib import Path
+from typing import Union
+
+
+def read_csv(path: Union[str, Path]) -> list[dict[str, str]]:
+    """Read a CSV file and return a list of dictionaries.
+
+    Because of how the DictReader works, every cell per row is just a string. If you want to
+    convert it to a python object, you'll need to parse it or use `ast.literal_eval`. That is why
+    the return type annotation is the way it is.
+    """
+    with open(path) as csvfile:
+        reader = csv.DictReader(csvfile)
+        data = list(reader)
+
+    return data
