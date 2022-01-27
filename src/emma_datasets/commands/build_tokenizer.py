a72631de086fe1223a8f57d6611fbe9bba14fb05
Alessandro Suglia <alessandro.suglia@gmail.com>
2022-01-27 17:17:54 +0000

    EMMA Tokenizer (#96)
    * Added tokenizers dependency
    
    * Create script to build EMMA tokenizer
    
    * Added extra check for DatasetDict; Task prefixes enum.
    
    * Added __str__ for task prefixes

---
+++ src/emma_datasets/commands/build_tokenizer.py
@@ -0,0 +1,87 @@
+import os
+from argparse import ArgumentParser, Namespace
+from typing import Iterable
+
+from tokenizers import Tokenizer
+from tokenizers.models import BPE, WordPiece
+from tokenizers.pre_tokenizers import Whitespace
+from tokenizers.trainers import BpeTrainer, WordPieceTrainer
+
+from emma_datasets.api.storage import DatasetDB
+from emma_datasets.common.logger import get_logger
+from emma_datasets.datamodels.instance import Instance
+
+
+logger = get_logger(__name__)
+
+
+def create_data_iterator(db_path: str) -> Iterable[str]:
+    """Opens the dataset and create an iterator over all the language annotations."""
+    with DatasetDB(db_path) as db:
+        for _, _, raw_data in db:
+            data = Instance.parse_raw(raw_data)
+
+            yield from data.language_annotations
+
+
+def main(args: Namespace) -> None:
+    """Trains a new tokenizer."""
+    logger.info(f"Loading data from dataset {args.db_path}")
+    data_iterator = create_data_iterator(args.db_path)
+    # The order of the special tokens matters!
+    special_tokens = ["[UNK]", "[CLS]", "[SEP]", "[PAD]", "[MASK]"]
+
+    if args.tokenizer == "bpe":
+        tokenizer = Tokenizer(BPE(unk_token=special_tokens[0]))
+        trainer = BpeTrainer(
+            vocab_size=args.vocab_size,
+            min_frequency=args.min_frequency,
+            special_tokens=special_tokens,
+        )
+    elif args.tokenizer == "wordpieces":
+        tokenizer = Tokenizer(WordPiece(unk_token=special_tokens[0]))
+        trainer = WordPieceTrainer(
+            vocab_size=args.vocab_size,
+            min_frequency=args.min_frequency,
+            special_tokens=special_tokens,
+        )
+    else:
+        raise ValueError(f"Wrong choice of Tokenizer: {args.tokenizer}")
+
+    # After we instantiate the tokenizer, we need to setup a pre-tokenizer
+    # to find correct word boundaries
+    tokenizer.pre_tokenizer = Whitespace()
+
+    logger.info(f"Created {type(tokenizer)} tokenizer")
+
+    tokenizer.train_from_iterator(data_iterator, trainer)
+
+    logger.info(f"Saving tokenizer to path {args.output_path}")
+    os.makedirs(os.path.dirname(args.output_path), exist_ok=True)
+    tokenizer.save(args.output_path)
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+
+    parser.add_argument(
+        "--db_path", type=str, help="Path to a DatasetDB file", default="storage/db/instances.db"
+    )
+    parser.add_argument(
+        "--tokenizer",
+        type=str,
+        default="bpe",
+        choices=("wordpieces", "bpe"),
+        help="The type of tokenizer to train",
+    )
+    parser.add_argument("--vocab_size", type=int, default=30000)  # noqa: WPS432
+    parser.add_argument("--min_frequency", type=int, default=0)
+    parser.add_argument(
+        "--output_path",
+        type=str,
+        help="Tokenizer output path",
+        default="storage/tokenizer/emma.json",
+    )
+
+    args = parser.parse_args()
+    main(args)
--- src/emma_datasets/datamodels/instance.py
+++ src/emma_datasets/datamodels/instance.py
@@ -1,3 +1,4 @@
+import re
 from pathlib import Path
 from typing import Optional, Union
 
@@ -6,11 +7,89 @@ from emma_datasets.datamodels.constants import DatasetModalityMap, DatasetName, 
 from emma_datasets.datamodels.dataset_metadata import DatasetMetadata
 from emma_datasets.datamodels.region import Region
 from emma_datasets.datamodels.scene_graph import SceneGraph
+from emma_datasets.datamodels.tasks import TaskPrefix
 from emma_datasets.datamodels.text import Caption, QuestionAnswerPair
 from emma_datasets.datamodels.trajectory import ActionTrajectory
 
 
 DatasetDict = dict[DatasetName, DatasetMetadata]
+
+
+def _get_language_data_from_scene_graph(scene_graph: SceneGraph) -> list[str]:
+    annotations = []
+
+    for _, scene_obj in scene_graph.objects.items():
+        if scene_obj.attributes:
+            for attr in scene_obj.attributes:
+                annotations.append(
+                    f"{TaskPrefix.object_attribute} {scene_obj.name} has attribute {attr}"
+                )
+
+        if scene_obj.relations:
+            for rel in scene_obj.relations:
+                rel_object = scene_graph.objects[rel.object]
+                annotations.append(
+                    f"{TaskPrefix.object_relation}: {scene_obj.name} {rel.name} {rel_object.name}"
+                )
+
+    return annotations
+
+
+def get_action_string(action_name: str) -> str:
+    """Returns a phrase associated with the action API name.
+
+    API action names are in camelcase format: MoveAhead_25
+    """
+    parts: list[str] = []
+
+    for x in re.findall("[A-Z][^A-Z]*", action_name):
+        parts.extend(xi for xi in x.split("_"))
+
+    return " ".join(parts)
+
+
+def _get_language_data_from_trajectory(trajectory: ActionTrajectory) -> list[str]:
+    trajectory_str = " ".join(
+        get_action_string(low_action.discrete_action.action)
+        for low_action in trajectory.low_level_actions
+    )
+    return [f"{TaskPrefix.execute_actions}: {trajectory_str}"]
+
+
+def _get_language_data_from_caption(caption: Caption) -> list[str]:
+    """Returns the caption text."""
+    return [f"{TaskPrefix.describe_scene}: {caption.text}"]
+
+
+def _get_language_data_from_qa(qa: QuestionAnswerPair) -> list[str]:
+    """Returns a formatted string containing both the question and the answer."""
+    return [f"{TaskPrefix.answer_question}: {qa.question} {qa.answer}"]
+
+
+def _get_language_data_from_regions(regions: list[Region]) -> list[str]:
+    """Returns the region descriptions for each region of the image."""
+    return [f"{TaskPrefix.describe_region}: {region.caption}" for region in regions]
+
+
+AnnotationType = Union[Caption, SceneGraph, list[Region], QuestionAnswerPair, ActionTrajectory]
+
+
+def get_language_data(
+    attribute_value: AnnotationType,
+) -> list[str]:
+    """Returns the language annotations associated with a given attribute."""
+    if isinstance(attribute_value, Caption):
+        return _get_language_data_from_caption(attribute_value)
+
+    if isinstance(attribute_value, QuestionAnswerPair):
+        return _get_language_data_from_qa(attribute_value)
+    if isinstance(attribute_value, list) and isinstance(attribute_value[0], Region):
+        return _get_language_data_from_regions(attribute_value)
+
+    if isinstance(attribute_value, SceneGraph):
+        return _get_language_data_from_scene_graph(attribute_value)
+
+    return _get_language_data_from_trajectory(attribute_value)
 
 
 class Instance(BaseModel):
@@ -37,6 +116,19 @@ class Instance(BaseModel):
         return next(iter(instance_modalities))
 
     @property
+    def language_annotations(self) -> list[str]:
+        """Derives all the language annotations associated with a given instance."""
+        lang_data_iterable = []
+
+        # iterates over the attributes of the class
+        for attribute_name, attribute_value in self:
+            # if the current attribute is available and it's not the `dataset` name
+            if attribute_name != "dataset" and attribute_value is not None:
+                lang_data = get_language_data(attribute_value)
+                lang_data_iterable.extend(lang_data)
+
+        return lang_data_iterable
+
     def paths(self) -> Union[Path, list[Path], None]:
         """Get source paths for this instance.
 
