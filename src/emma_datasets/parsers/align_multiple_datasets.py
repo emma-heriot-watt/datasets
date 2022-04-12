b63faa478838d7ea6cf8b0d001cbaaa5fcf0bec4
Amit Parekh <7276308+amitkparekh@users.noreply.github.com>
2022-04-12 16:01:12 +0100

    Fix getting duplicate metadata during alignment (#333)
    * ops: dont care about cognitive complexity in tests
    
    * fix: instance attribute names with the creator not adding in qa pairs properly
    
    * ops: fix the comma in the flake8 config
    
    * Update test_instance_creator.py
    
    * fix: types
    
    * fix: incorrect casting
    
    * something wrong
    
    * fix: getting duplicates of common dataset metadata
    
    * Delete data_counting.py
    
    * Update settings.py
    
    * fix: reenable all metadata parsers

--- src/emma_datasets/parsers/align_multiple_datasets.py
+++ src/emma_datasets/parsers/align_multiple_datasets.py
@@ -26,8 +26,7 @@ class AlignMultipleDatasets:
         self.task_id = progress.add_task(description, start=False, visible=False, comment="")
 
     def __call__(
-        self,
-        *aligned_metadata_iterable: DatasetAlignerReturn,
+        self, *aligned_metadata_iterable: DatasetAlignerReturn
     ) -> Iterator[list[DatasetMetadata]]:
         """Align metadata across the multiple datasets to a signle common dataset.
 
@@ -44,7 +43,10 @@ class AlignMultipleDatasets:
             for aligned_metadata in aligned_metadata_iterable
         ]
 
-        aligned_ids = self.get_aligned_ids(all_common_dataset_mapping)
+        aligned_common_ids = self.get_common_aligned_ids(all_common_dataset_mapping)
+        non_aligned_common_ids = self.get_non_aligned_common_ids(
+            all_common_dataset_mapping, aligned_common_ids
+        )
 
         self.progress.reset(
             self.task_id,
@@ -53,11 +55,15 @@ class AlignMultipleDatasets:
             visible=True,
         )
 
-        common_instances = self.get_all_common_instances(all_common_dataset_mapping, aligned_ids)
-        non_common_instances = self.get_all_non_common_instances(
-            all_common_dataset_mapping, aligned_ids
+        common_instances = self.get_all_common_instances(
+            all_common_dataset_mapping, aligned_common_ids
         )
-        non_aligned_instances = self.get_all_non_alignable_instances(aligned_metadata_iterable)
+        non_common_instances = self.get_all_non_common_instances(
+            all_common_dataset_mapping, aligned_common_ids
+        )
+        non_aligned_instances = self.get_all_non_alignable_instances(
+            aligned_metadata_iterable, non_aligned_common_ids
+        )
 
         return itertools.chain(common_instances, non_common_instances, non_aligned_instances)
 
@@ -85,18 +91,47 @@ class AlignMultipleDatasets:
                 yield list(mapping[non_common_id].values())
 
     def get_all_non_alignable_instances(
-        self, aligned_metadata_iterable: Iterable[DatasetAlignerReturn]
+        self, aligned_metadata_iterable: Iterable[DatasetAlignerReturn], non_common_ids: set[str]
     ) -> Iterator[list[DatasetMetadata]]:
         """Get all instances which cannot be aligned."""
         non_aligned_metadata = (metadata.non_aligned for metadata in aligned_metadata_iterable)
 
+        existing_common_dataset_ids = set() | non_common_ids
+
         for non_aligned in itertools.chain.from_iterable(non_aligned_metadata):
             self.progress.advance(self.task_id)
+
+            if self.common_dataset in non_aligned:
+                metadata_id = non_aligned[self.common_dataset].id  # noqa: WPS529
+
+                if metadata_id in existing_common_dataset_ids:
+                    continue
+
+                existing_common_dataset_ids.add(metadata_id)
+
             yield list(non_aligned.values())
 
-    def get_aligned_ids(self, all_common_dataset_mapping: list[CommonDatasetMapping]) -> set[str]:
-        """Get IDs of instances from the common dataset which are aligned across all datasets."""
+    def get_common_aligned_ids(
+        self, all_common_dataset_mapping: list[CommonDatasetMapping]
+    ) -> set[str]:
+        """Get IDs of instances from the common dataset which are aligned across all datasets.
+
+        The IDs returned are for the `self.common_dataset`.
+        """
         return set.intersection(*[set(mapping.keys()) for mapping in all_common_dataset_mapping])
+
+    def get_non_aligned_common_ids(
+        self, all_common_dataset_mapping: list[CommonDatasetMapping], aligned_common_ids: set[str]
+    ) -> set[str]:
+        """Get instance IDs which are aligned to the common dataset but not across all datasets.
+
+        In other words, this returns a set of IDs from the common dataset which ARE aligned to one
+        other dataset, but NOT ALL of the other datasets.
+        """
+        all_non_overlapping_ids = [
+            set(mapping.keys()) - aligned_common_ids for mapping in all_common_dataset_mapping
+        ]
+        return set.union(*all_non_overlapping_ids)
 
     def _get_mapping_to_common_dataset(
         self, aligned_metadata: DatasetAlignerReturn
@@ -111,11 +146,31 @@ class AlignMultipleDatasets:
         all_common_dataset_mapping: list[CommonDatasetMapping],
         aligned_metadata_iterable: Iterable[DatasetAlignerReturn],
     ) -> int:
-        aligned_ids = self.get_aligned_ids(all_common_dataset_mapping)
-        non_overlapping_ids = [
-            len(set(mapping.keys()) - aligned_ids) for mapping in all_common_dataset_mapping
-        ]
-        non_aligned_metadata = (
-            len(metadata.non_aligned) for metadata in aligned_metadata_iterable
+        """Calculate total number of instances that will be returned."""
+        aligned_common_ids = self.get_common_aligned_ids(all_common_dataset_mapping)
+
+        common_ids_aligned_to_other_dataset = self.get_non_aligned_common_ids(
+            all_common_dataset_mapping, aligned_common_ids
         )
-        return len(aligned_ids) + sum(non_overlapping_ids) + sum(non_aligned_metadata)
+
+        common_ids_not_aligned_to_any_dataset = {
+            metadata_dict[self.common_dataset].id
+            for metadata_list in aligned_metadata_iterable
+            for metadata_dict in metadata_list.non_aligned
+            if self.common_dataset in metadata_dict
+            and metadata_dict[self.common_dataset].id not in common_ids_aligned_to_other_dataset
+        }
+
+        other_instances_not_aligned_to_common_dataset = [
+            metadata_dict
+            for metadata_list in aligned_metadata_iterable
+            for metadata_dict in metadata_list.non_aligned
+            if self.common_dataset not in metadata_dict
+        ]
+
+        return (
+            len(aligned_common_ids)
+            + len(common_ids_aligned_to_other_dataset)
+            + len(common_ids_not_aligned_to_any_dataset)
+            + len(other_instances_not_aligned_to_common_dataset)
+        )
